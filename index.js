da39bd9e1cda4143bfee3a67409b6105c8be0f8f
+ <+>
2022-06-11 21:13:45 +0300

    commit tree 31f2cf7570715ef4280834ce20630f5c9efaa159

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(action = 'roller-skate') {
+    return `This Saturday, I want to ${action}!`;
+}
+
+function mondayWork(action = 'go to the office') {
+    return `This Monday, I will ${action}.`;
+}
+
+function wrapAdjective(flair = '*') {
+    return function(param = 'special') {
+        return `You are ${flair}${param}${flair}!`;
+    }
+}
